98a22d76ac3d60652939d1adf771e04471664e1e
Mickael GOETZ <mickael.goetz@gmail.com>
2012-03-11 21:23:40 +0100

    Added BaseAPI for a better inheritance organisation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 __author__ = 'MrMitch'
 
 from utils.TempConverter import IMPERIAL_SYSTEM, METRIC_SYSTEM
-from providers.GoogleAPI import GoogleAPI
+from APIs.Google import GoogleAPI
 from sys import argv, stdout
 
 location = 'New York'
@@ -45,12 +45,19 @@ try:
     print '\n## Current weather in %s ##' % weather.fullLocation()
     print 'Temp: %s' % weather.temp()
     print 'Humidity: %s' % weather.humidity()
-    print 'Condition (full): %s' % weather.textualCondition()
-    print 'Condition (symbolic): %s' % weather.symbolicCondition()
+    print 'Condition (full): %s' % weather.text()
+    print 'Condition (symbolic): %s' % weather.symbol()
     print '\n## Forecast for %s ##' % location
     print '     '.join(weather.forecastDaysList())
     print ' '.join(weather.forecastTemperaturesList())
     print ' ' + '       '.join(weather.forecastSymbolsList())
+
+    future = 2
+    
+    print '\nin %i days we are: ' % future + weather.forecastDay(future)
+    print 'forecast temps are: ' + weather.forecastTemperatures(future)
+    print 'forecast condition is: ' + weather.forecastText(future)
+    print 'forecast symbol is: ' + weather.forecastSymbol(future)
 
 except Exception:
     print 'Wrong parameters. Try "./main.py -h" or "./main.py --help" for more information.'
